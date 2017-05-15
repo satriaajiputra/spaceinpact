716283a10383e6275b04e4437d729510a811dc1c
Satria Aji Putra <satriamaxt@gmail.com>
2017-05-15 19:59:23 +0700

    change image

--- alien.js
+++ alien.js
@@ -1,10 +1,11 @@
 class Alien {
 	constructor(canvas)
 	{
-		this.image = 'alien.png';
+		this.image = 'alien2.png';
 		this.height = 40;
 		this.width = 50;
 		this.canvas = canvas;
+		
 		this.drawObject();
 	}
 
@@ -12,10 +13,12 @@ class Alien {
 	{	
 		let ctx = this.canvas.getContext('2d');
 		let img = new Image();
-
+		let clientX = 0;
+		
 		img.onload = function() {
-			ctx.drawImage(img, 0, -35, 64, 256, 0, 0, 50, 40);
+			ctx.drawImage(img, 0,0,50,40,0,0,50,40);
 		}
-		img.src= 'alien.png';
+
+		img.src= this.image;
 	}
 }
