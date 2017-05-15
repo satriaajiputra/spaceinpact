bac954d5cd8f6492057047cd228b9c94531a5f8f
nugraha <mangcoding@gmail.com>
2017-05-15 22:31:34 +0700

    update games

--- alien.js
+++ alien.js
@@ -1,24 +1,49 @@
 class Alien {
-	constructor(canvas)
+	constructor(canvas,width,height)
 	{
 		this.image = 'alien2.png';
-		this.height = 40;
-		this.width = 50;
+		this.height = height;
+		this.width = width;
 		this.canvas = canvas;
-		
-		this.drawObject();
+		this.speed = 300;
+		this.margin = 6; //margin on sprites
+		this.alien = 1;
+		this.startFrame = 0;
+		this.score = 10;
 	}
 
+	setImage(image) {
+		this.image = image;
+	}
+
+	setAlien(alien) {
+		this.alien = alien;
+	}
+
+	cekFrame() {
+		if (this.alien == 1) {
+			this.startFrame = 0;
+		}else if (this.alien == 2) {
+			this.startFrame = 2;
+		}
+		else if (this.alien == 3) {
+			this.startFrame = 4;
+		}else{
+			this.startFrame = 0;
+		}
+	}
 	drawObject()
 	{	
-		let ctx = this.canvas.getContext('2d');
-		let img = new Image();
-		let clientX = 0;
-		
-		img.onload = function() {
-			ctx.drawImage(img, 0,0,50,40,0,0,50,40);
-		}
-
-		img.src= this.image;
+		this.cekFrame();
+		let ctx = this.canvas;
+		let plusX = this.startFrame*(this.width+this.margin);
+		let base = this;
+		let frame = 0;
+		setInterval(function () {
+			ctx.style.backgroundImage = 'url('+base.image+')';
+			let positionX = (frame % 2)*base.width+plusX+(frame % 2)*base.margin;
+			ctx.style.backgroundPosition = -positionX+"px 0px";
+			frame++;
+		},base.speed);
 	}
 }
